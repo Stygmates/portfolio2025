a2920ace766b0e70722617b941ae157fe9f583c0
Tan Dat Truong <truongt@igbmc.fr>
2025-04-10 22:34:23 +0200

    Refactor code

--- src/app/components/HobbyStack.tsx
+++ src/app/components/HobbyStack.tsx
@@ -1,15 +1,36 @@
 import Image from "next/image"
 
+
+const hobbies = [
+    { src: "/volley.svg", alt: "Volley-ball", label: "Volley-ball" },
+    { src: "/pingpong.svg", alt: "pingpong", label: "Table tennis" },
+    { src: "/videogames.svg", alt: "Video games", label: "Video games" },
+    { src: "/workingout.svg", alt: "Gym", label: "Gym" },
+];
+
 export default function HobbyStack() {
     return (
         <div id="techstack" className="flex flex-col items-center justify-center h-full">
             <h1 className="text-5xl font-bold">My current Hobby Stack</h1>
             <div className="flex flex-wrap gap-4 mt-8">
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/volley.svg" alt="Volley-ball" width={48} height={48}/>Volley-ball</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/pingpong.svg" alt="pingpong" width={48} height={48}/>Table tennis</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/videogames.svg" alt="Video games" width={48} height={48}/>Video games</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/workingout.svg" alt="Gym" width={48} height={48}/>Gym</div>
+                {hobbies.map((hobby) => (
+                    <HobbyCard key={hobby.alt} {...hobby} />
+                ))}
             </div>
         </div>
     )
 }
+
+
+type HobbyCardProps = {
+    src: string;
+    alt: string;
+    label: string;
+  };
+  
+  export const HobbyCard = ({ src, alt, label }: HobbyCardProps) => (
+    <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110 transition-all">
+      <Image src={src} alt={alt} width={48} height={48} />
+      {label}
+    </div>
+  );
--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -1,15 +1,35 @@
 import Image from "next/image"
 
+const technologies = [
+    { src: '/rust.svg', alt: 'Rust', label: 'Rust' },
+    { src: '/nextjs.svg', alt: 'Next.js', label: 'NextJS' },
+    { src: '/tailwind.svg', alt: 'Tailwind CSS', label: 'TailwindCSS' },
+    { src: '/docker.svg', alt: 'Docker', label: 'Docker' },
+];
+
 export default function TechStack() {
     return (
         <div id="techstack" className="flex flex-col items-center justify-center mt-32 md:mt-128 h-full">
             <h1 className="text-5xl font-bold">My current Tech Stack</h1>
             <div className="flex flex-wrap gap-4 mt-8">
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/rust.svg" alt="Rust" width={48} height={48} />Rust</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/nextjs.svg" alt="Next.js" width={48} height={48}/> NextJS</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/tailwind.svg" alt="Tailwind CSS" width={48} height={48} />TailwindCSS</div>
-                <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110"><Image src="/docker.svg" alt="Docker" width={48} height={48}/>Docker</div>
+                {technologies.map((tech) => (
+                <TechCard key={tech.alt} {...tech} />
+                ))}
             </div>
         </div>
     )
 }
+
+
+type TechCardProps = {
+    src: string;
+    alt: string;
+    label: string;
+};
+  
+export const TechCard = ({ src, alt, label }: TechCardProps) => (
+    <div className="border border-solid rounded-md flex flex-col gap-4 justify-center items-center font-bold w-32 h-32 hover:brightness-125 hover:scale-110 transition-all">
+      <Image src={src} alt={alt} width={48} height={48} />
+      {label}
+    </div>
+);
