911e0ab5bb01358d74ab2fbfc5580a46ac3a45af
Tan Dat Truong <truongt@igbmc.fr>
2025-04-11 16:51:57 +0200

    Add landing page animation

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -18,7 +18,7 @@ export default function AboutMe() {
     }, [texts.length]);
     
     return(
-    <div className="flex font-bold flex-col items-center justify-center mt-32 md:mt-96 h-full">
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 5}} className="flex font-bold flex-col items-center justify-center mt-64 md:mt-96 mx-8">
      <div className="text-5xl text-center">Hello world,<br/>I&apos;m <span className="text-blue-400">Tan Dat</span>.
         <AnimatePresence mode="wait" >
         <motion.div className="inline" initial={{ opacity: 0 }} animate={{ opacity: [0, 1, 0] }} transition={{ duration: 1, repeat: Infinity }}>|</motion.div>
@@ -30,8 +30,8 @@ export default function AboutMe() {
         stuff.</div>
         <div className="text-xl text-center">
             <span>I&apos;m a software engineer from Strasbourg, France. I build apps, nowadays mainly</span>
-            <Image src="/rust.svg" alt="Rust" width={24} height={24} className="inline align-baseline mx-1"/><span> apps.</span>
+            <Image src="/rust.svg" alt="Rust" width={24} height={24} className="inline align-baseline mx-1"/><span> and <span> <Image src="/nextjs.svg" alt="NextJS" width={24} height={24} className="inline align-baseline mx-1"/></span>apps.</span>
         </div>
         <a href="mailto:admin@tdtruong.com" className="border border-solid border-blue-400 rounded-md p-4 text-blue-400 my-4 hover:brightness-125"><EmailIcon sx={{ color: 'oklch(70.7% 0.165 254.624)' }} /> Say hi! </a>
-    </div>)
+    </motion.div>)
 }
